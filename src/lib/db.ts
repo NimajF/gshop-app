6f4911f30fd14a0665b234c601e640b6d74c353d
NimajF <benjafran2000@gmail.com>
2025-05-06 21:52:22 -0300

    config

---
+++ src/lib/db.ts
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose';
+
+const MONGODB_URI: string | undefined = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+    throw new Error(
+        'Please define the MONGODB_URI environment variable inside .env.local'
+    );
+}
+
+/**
+ * Global is used here to maintain a cached connection across hot reloads
+ * in development. This prevents connections growing exponentially
+ * during API Route usage.
+ */
+// @ts-expect-error
+let cached = global.mongoose;
+
+if (!cached) {
+    // @ts-expect-error
+    cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectDB() {
+    if (cached.conn) {
+        return cached.conn;
+    }
+
+    if (!cached.promise) {
+        cached.promise = mongoose.connect(MONGODB_URI as string).then((mongoose) => {
+            return mongoose;
+        });
+    }
+
+    try {
+        cached.conn = await cached.promise;
+    } catch (e) {
+        cached.promise = null;
+        throw e;
+    }
+
+    return cached.conn;
+}
+
+export default connectDB;
