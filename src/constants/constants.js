da0f95a377767eda0e4f3bd6b20f42a57018b1aa
NimajF <benjafran2000@gmail.com>
2025-05-12 21:26:13 -0300

    Add constants

---
+++ src/constants/constants.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.categories = void 0;
+exports.categories = [
+    { name: "FERTILIZANTES", slug: "fertilizantes" },
+    { name: "PREVENCIÓN", slug: "prevencion" },
+    { name: "SUSTRATOS", slug: "sustratos" },
+    { name: "MACETAS", slug: "macetas" },
+    { name: "INDOOR", slug: "indoor" },
+    { name: "HERRAMIENTAS", slug: "herramientas" },
+    { name: "INDUMENTARIA Y OTROS", slug: "indumentaria-y-otros" },
+    { name: "PARAFERNALIA", slug: "parafernalia" },
+    { name: "ESQUEJES", slug: "esquejes" },
+    { name: "SEMILLAS", slug: "semillas" },
+];
---
+++ src/constants/constants.ts
@@ -0,0 +1,12 @@
+export const categories = [
+    { name: "FERTILIZANTES", slug: "fertilizantes" },
+    { name: "PREVENCIÓN", slug: "prevencion" },
+    { name: "SUSTRATOS", slug: "sustratos" },
+    { name: "MACETAS", slug: "macetas" },
+    { name: "INDOOR", slug: "indoor" },
+    { name: "HERRAMIENTAS", slug: "herramientas" },
+    { name: "INDUMENTARIA Y OTROS", slug: "indumentaria-y-otros" },
+    { name: "PARAFERNALIA", slug: "parafernalia" },
+    { name: "ESQUEJES", slug: "esquejes" },
+    { name: "SEMILLAS", slug: "semillas" },
+];
